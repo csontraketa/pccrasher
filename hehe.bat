5dad031266a3676608aee386dd49a8de74a44d87
CsontRAKETA <130133174+csontraketa@users.noreply.github.com>
2025-02-01 16:23:37 +0100

    Download if ur an idiot!!! ^^^^
    sus

---
+++ hehe.bat
@@ -0,0 +1,38 @@
+echo off
+setlocal
+
+set /A szamok = 01
+chcp 65001
+
+cls
+color 02
+start https://i.insider.com/61135525ad63f30019501966?width=700&format=jpeg&auto=webp
+echo PC CRASHER
+echo ###############################
+echo # SUSSY BAKA VIRUS: ACTIVATED #
+echo # STARTING IN 10...           #
+echo # BY CSONTRAKETA        :(    #
+echo ###############################
+
+TIMEOUT /T 5 /NOBREAK
+
+:cirkusz
+
+color %szamok%
+
+echo HEHEHE  HEHEH    c ontrak ta.git ub.io
+echo HEHEH  HEHEH  H   sontra eta.gi hub.io              
+echo HEHE  EHEHE  EH  csontr keta.g thub.i 
+echo HEH  HEHEH  HEH  csont aketa. ithub. o
+echo HE  EHEHE  EHEH  cson raketa github io
+echo H  HEHEH  HEHEH  cso traket .githu .io
+echo   EHEHE  EHEHEH  cs ntrake a.gith b.io
+
+set /A szamok +=1
+
+if %szamok% == 100 (set /A szamok=01)
+REM cls
+
+goto cirkusz
+
+endlocal
