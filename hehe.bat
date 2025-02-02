8341036c4806914421814c66f5a956a093902c5d
CsontRAKETA <130133174+csontraketa@users.noreply.github.com>
2025-02-02 10:27:39 +0100

    Update hehe.bat - New features - RickRoll switch (line 09)
    - RickRoll switch
    - Payment
    - Shorter wait time
    - Others...

--- hehe.bat
+++ hehe.bat
@@ -6,26 +6,30 @@ chcp 65001
 
 cls
 color 02
-start https://i.insider.com/61135525ad63f30019501966?width=700&format=jpeg&auto=webp
-echo PC CRASHER
+REM start https://i.insider.com/61135525ad63f30019501966?width=700&format=jpeg&auto=webp  <-- ACTIVATE FOR RICKROLL
+ECHO ###############################
+echo #         PC CRASHER          #
 echo ###############################
-echo # SUSSY BAKA VIRUS: ACTIVATED #
-echo # STARTING IN 10...           #
-echo # BY CSONTRAKETA        :(    #
+echo # VIRUS STATUS :           01 #
+echo # YOUR  STATUS :           01 #
+echo # STARTING IN  :           03 #
+echo # WRITTEN  IN  :        BATCH #
+echo # BY           :  CSONTRAKETA #
+echo #  FIZESS 10K 113 CSONTRAKETA #
 echo ###############################
 
-TIMEOUT /T 5 /NOBREAK
+TIMEOUT /T 3 /NOBREAK
 
 :cirkusz
 
 color %szamok%
 
-echo HEHEHE  HEHEH    c ontrak ta.git ub.io
-echo HEHEH  HEHEH  H   sontra eta.gi hub.io              
-echo HEHE  EHEHE  EH  csontr keta.g thub.i 
-echo HEH  HEHEH  HEH  csont aketa. ithub. o
-echo HE  EHEHE  EHEH  cson raketa github io
-echo H  HEHEH  HEHEH  cso traket .githu .io
+echo HEHEHE  HEHEH    c ontrak ta.git ub.io 
+echo HEHEH  HEHEH  H   sontra eta.gi hub.io         
+echo HEHE  EHEHE  EH  csontr keta.g thub.i  
+echo HEH  HEHEH  HEH  csont aketa. ithub. o 
+echo HE  EHEHE  EHEH  cson raketa github io  
+echo H  HEHEH  HEHEH  cso traket .githu .io  
 echo   EHEHE  EHEHEH  cs ntrake a.gith b.io
 
 set /A szamok +=1
